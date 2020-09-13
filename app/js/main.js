afa7f1dc43584a38ff6036d04592924f4094a8e4
TheSplatt-WEB <b-26_sokol@mail.ru>
2020-09-14 03:38:47 +0800

    added new-items > search, breadcrumps, asaid

--- app/js/main.js
+++ app/js/main.js
@@ -2,18 +2,29 @@ $(function(){
 
     $('.rate-star').rateYo({
       rating: 5,
-      starWidth: '12px',
+      starWidth: '11px',
       ratedFill: '#ffa726',
       readOnly: true,
       spacing: '2px'
     });
 
-    var mixer = mixitup('.products__inner-box');
+    if($('div').is('.mix')){
+      var mixer = mixitup('.products__inner-box');
+    };
 
     $('.product-slider__inner').slick({
       arrows: false,
       dots: true,
       slidesToShow: 4,
       slidesToScroll: 4,
+    });
+
+    $('.js-range-slider').ionRangeSlider({
+      type: 'double',
+      min: 0,
+      max: 1000,
+      from: 0,
+      to: 600,
+      prefix: '$',
     });
 });
