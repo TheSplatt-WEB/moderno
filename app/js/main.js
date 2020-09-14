f5b953345be8fab9daa1016ba6e7f83e0704e7be
TheSplatt-WEB <b-26_sokol@mail.ru>
2020-09-14 20:34:16 +0800

    added new items > page-content, added all icons in header and tranding icons in all pages

--- app/js/main.js
+++ app/js/main.js
@@ -27,4 +27,15 @@ $(function(){
       to: 600,
       prefix: '$',
     });
+
+    $('.icon-th-large').on('click', function(){
+      $('.products__item').removeClass('list');
+      $('.icon-th-large').addClass('active');
+      $('.icon-th-list').removeClass('active');
+    });
+    $('.icon-th-list').on('click', function(){
+      $('.products__item').addClass('list');
+      $('.icon-th-list').addClass('active');
+      $('.icon-th-large').removeClass('active');
+    });
 });
